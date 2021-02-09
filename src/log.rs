86e2ca54bffafa15e1266712303e22cef0d53b45
qbx2 <sunyeop97@gmail.com>
2021-02-09 22:01:02 +0900

    Reindent using spaces

--- src/log.rs
+++ src/log.rs
@@ -13,34 +13,34 @@ const LOG_PATH: &str = "\\My Games\\Skyrim Special Edition\\SKSE\\hide-remote-de
 static mut LOG: Option<LineWriter<File>> = None;
 
 pub(crate) unsafe fn open_log_file() -> Result<LineWriter<File>, Box<dyn std::error::Error>> {
-	let mut path = Vec::with_capacity(MAX_PATH);
-	let result = SHGetFolderPathA(
-		NULL as HWND,
-		CSIDL_MYDOCUMENTS | CSIDL_FLAG_CREATE,
-		NULL,
-		SHGFP_TYPE_CURRENT,
-		path.as_mut_ptr(),
-	);
-	if result != S_OK {
-		return Err(std::io::Error::new(
-			std::io::ErrorKind::Other,
-			format!("failed to SHGetFolderPathA, ret = {}, err = {}", result, GetLastError()),
-		).into());
-	}
+    let mut path = Vec::with_capacity(MAX_PATH);
+    let result = SHGetFolderPathA(
+        NULL as HWND,
+        CSIDL_MYDOCUMENTS | CSIDL_FLAG_CREATE,
+        NULL,
+        SHGFP_TYPE_CURRENT,
+        path.as_mut_ptr(),
+    );
+    if result != S_OK {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("failed to SHGetFolderPathA, ret = {}, err = {}", result, GetLastError()),
+        ).into());
+    }
 
-	let path = String::from(CStr::from_ptr(path.as_ptr()).to_str()?) + LOG_PATH;
+    let path = String::from(CStr::from_ptr(path.as_ptr()).to_str()?) + LOG_PATH;
 
-	let file = File::create(&path)?;
-	Ok(LineWriter::new(file))
+    let file = File::create(&path)?;
+    Ok(LineWriter::new(file))
 }
 
 pub(crate) fn get_log() -> &'static mut LineWriter<File> {
-	unsafe {
-		return if let Some(ref mut log) = LOG {
-			log
-		} else {
-			LOG = Some(open_log_file().unwrap());
-			LOG.as_mut().unwrap()
-		}
-	}
+    unsafe {
+        return if let Some(ref mut log) = LOG {
+            log
+        } else {
+            LOG = Some(open_log_file().unwrap());
+            LOG.as_mut().unwrap()
+        }
+    }
 }
